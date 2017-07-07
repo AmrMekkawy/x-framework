26da922c27d8de2ad76947a8a3899b62b922187e
Amr Mekkawy <amr.mekkawy@gmail.com>
2017-07-07 04:14:41 +0200

    Create gulpfile.js file, install gulp-sass plugin and write a gulp task for it in gulpfile.js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
+// Gulp for beginners: https://goo.gl/ZVJvBt
+
 // The require statement tells Node to look into the node_modules folder
 // for a package named gulp. Once the package is found, 
 // we assign its contents to the variable gulp. more info: https://goo.gl/ZVJvBt
 var gulp = require('gulp');
+
+// --------------------------------------------------------------------
+
+// Requires the gulp-sass plugin
+var gulpSass = require('gulp-sass');
 
 // --------------------------------------------------------------------
 
@@ -14,11 +21,35 @@ gulp.task('task-name', function() {
         .pipe(aGulpPlugin()) // Sends it through a gulp plugin
         .pipe(gulp.dest('destination')); // Outputs the file in the destination folder
 });
-*/
 
 // We can now begin to write a gulp task with this gulp variable.
 gulp.task('hello', function() {
     console.log('Hello Zell');
 });
+*/
 
 // --------------------------------------------------------------------
+
+// Converts Sass to CSS with gulp-sass plugin
+gulp.task('sass', function() {
+    return gulp.src([
+            'src/scss/test.scss',
+            'src/scss/x-framework.scss'
+        ])
+        .pipe(gulpSass({
+                outputStyle: 'compact' // nested, expanded, compact, compressed
+            })
+            .on('error', gulpSass.logError)
+        )
+        .pipe(gulp.dest('dist/css'));
+});
+
+// --------------------------------------------------------------------
+
+// Watch files for changes
+gulp.task('watch', function() {
+    // Gets all files ending with .scss in src/scss and children dirs
+    gulp.watch(['src/scss/**/*.scss'], ['sass']);
+
+    // Other watchers
+});
