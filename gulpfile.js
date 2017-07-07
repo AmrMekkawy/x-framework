3f62a7e52b21ec7102497ec663d1846ea23dd282
Amr Mekkawy <amr.mekkawy@gmail.com>
2017-07-07 21:43:56 +0200

    Add gulp-concat and browser-sync gulp plugins

--- gulpfile.js
+++ gulpfile.js
@@ -5,51 +5,70 @@
 // we assign its contents to the variable gulp. more info: https://goo.gl/ZVJvBt
 var gulp = require('gulp');
 
+// gulp-sass: converts Sass to CSS
+var sass = require('gulp-sass');
+
+// gulp-concat: helps concatinating files
+var concat = require('gulp-concat');
+
+// browser-sync: helps make web development easier by spinning up a web server that helps us do live-reloading easily
+var browserSync = require('browser-sync').create();
+
 // --------------------------------------------------------------------
 
-// Requires the gulp-sass plugin
-var gulpSass = require('gulp-sass');
-
-// --------------------------------------------------------------------
-
-/*
-// As you can see, a real task takes in two additional gulp methods — gulp.src and gulp.dest.
-// gulp.src tells the Gulp task what files to use for the task, 
-// while gulp.dest tells Gulp where to output the files once the task is completed.
-gulp.task('task-name', function() {
-    return gulp.src('source-files') // Get source files with gulp.src
-        .pipe(aGulpPlugin()) // Sends it through a gulp plugin
-        .pipe(gulp.dest('destination')); // Outputs the file in the destination folder
+// live-reloading with browser-sync plugin
+// more info about browser-sync plugin see: https://goo.gl/P3Bz4u
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'src'
+        },
+    });
 });
-
-// We can now begin to write a gulp task with this gulp variable.
-gulp.task('hello', function() {
-    console.log('Hello Zell');
-});
-*/
 
 // --------------------------------------------------------------------
 
 // Converts Sass to CSS with gulp-sass plugin
+// more info about gulp-sass plugin see: https://goo.gl/XSwgu9
 gulp.task('sass', function() {
     return gulp.src([
             'src/scss/test.scss',
             'src/scss/x-framework.scss'
         ])
-        .pipe(gulpSass({
-                outputStyle: 'compact' // nested, expanded, compact, compressed
+        .pipe(sass({
+                outputStyle: 'compact' // available values: nested, expanded, compact, compressed
             })
-            .on('error', gulpSass.logError)
+            .on('error', sass.logError)
         )
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({ // because we use browser-sync plugin
+            stream: true
+        }));
 });
 
 // --------------------------------------------------------------------
 
-// Watch files for changes
-gulp.task('watch', function() {
-    // Gets all files ending with .scss in src/scss and children dirs
-    gulp.watch(['src/scss/**/*.scss'], ['sass']);
+// more info about gulp-concat plugin see: https://goo.gl/N1dbLD
+gulp.task('concatJS', function() {
+    return gulp.src([
+            'src/js/file1.js',
+            'src/js/file2.js',
+            'src/js/file3.js'
+        ])
+        .pipe(concat('build.js'))
+        .pipe(gulp.dest('dist/js'));
+});
 
-    // Other watchers
+// --------------------------------------------------------------------
+
+// It'll be cumbersome to open up two command line windows and run gulp browserSync 
+// and gulp watch separately, so let's get Gulp to run them together by telling the 
+// watch task that browserSync must be completed before watch is allowed to run.
+gulp.task('watch', ['browserSync', 'sass'], function() {
+    // Gets all files ending with .scss in src/scss and children dirs
+    gulp.watch('src/scss/**/*.scss', ['sass']);
+
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('src/*.html', browserSync.reload);
+    gulp.watch('src/js/**/*.js', ['concatJS', browserSync.reload]);
 });
