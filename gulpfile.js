21e6dff70c20af892fccbd265997819eb72dd4cf
Amr Mekkawy <amr.mekkawy@gmail.com>
2017-07-08 18:06:43 +0200

    Add 2 gulp plugins: gulp-imagemin to minify images & gulp-newer to passing through only newer source files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
 // Gulp for beginners: https://goo.gl/ZVJvBt
 
+// define locations of used directories
+var dirs = {
+    src: {
+        scss: 'src/scss',
+        js: 'src/js',
+        img: 'src/img'
+    },
+    dist: {
+        css: 'dist/css',
+        js: 'dist/js',
+        img: 'dist/img'
+    }
+};
+
 // The require statement tells Node to look into the node_modules folder
 // for a package named gulp. Once the package is found, 
 // we assign its contents to the variable gulp. more info: https://goo.gl/ZVJvBt
@@ -15,7 +29,7 @@ var concat = require('gulp-concat');
 var cssnano = require('gulp-cssnano');
 
 // gulp-rename: helps renaming files
-var rename = require("gulp-rename");
+var rename = require('gulp-rename');
 
 // run-sequence: helps run a series of dependent gulp tasks in order https://goo.gl/24fhj4
 var runSequence = require('run-sequence')
@@ -23,22 +37,30 @@ var runSequence = require('run-sequence')
 // gulp-clean: helps removing files and folders https://goo.gl/2eLLuk
 var clean = require('gulp-clean');
 
+// gulp-newer: A Gulp plugin for passing through only those source files that are 
+// newer than corresponding destination files. more info: https://goo.gl/16jiiw
+var newer = require('gulp-newer');
+
+// gulp-imagemin: helps minify PNG, JPEG, GIF and SVG images
+// more info: https://goo.gl/uFVm8v
+var imagemin = require('gulp-imagemin');
+
 // --------------------------------------------------------------------
 
 // Converts Sass to CSS with gulp-sass plugin
 // more info about gulp-sass plugin see: https://goo.gl/XSwgu9
 gulp.task('sass', function() {
     return gulp.src([
-            // 'src/scss/x-framework.scss',
-            'src/scss/test-1.scss',
-            'src/scss/test-2.scss'
+            // dirs.src.scss + '/x-framework.scss',
+            dirs.src.scss + '/test-1.scss',
+            dirs.src.scss + '/test-2.scss'
         ])
         .pipe(sass({
                 outputStyle: 'compact' // available values: nested, expanded, compact, compressed
             })
             .on('error', sass.logError)
         )
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest(dirs.dist.css));
 });
 
 // --------------------------------------------------------------------
@@ -49,46 +71,56 @@ gulp.task('sass', function() {
 // concatinating javascript files
 gulp.task('concatJS', function() {
     return gulp.src([
-            'src/js/file1.js',
-            'src/js/file2.js',
-            'src/js/file3.js'
+            dirs.src.js + '/file1.js',
+            dirs.src.js + '/file2.js',
+            dirs.src.js + '/file3.js'
         ])
         .pipe(concat('build.js'))
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest(dirs.dist.js));
 });
 */
 
 // concatinating css files
 gulp.task('concatCSS', function() {
     return gulp.src([
-            // 'src/css/x-framework.css',
-            'dist/css/test-1.css',
-            'dist/css/test-2.css'
+            // dirs.dist.css + '/x-framework.css',
+            dirs.dist.css + '/test-1.css',
+            dirs.dist.css + '/test-2.css'
         ])
         .pipe(concat('build.css'))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest(dirs.dist.css));
 });
 
 // --------------------------------------------------------------------
 
-
 // more info see: https://goo.gl/yhYoKc & https://goo.gl/qWA4T6
 gulp.task('minifyCSS', function() {
-    return gulp.src('dist/css/*.css')
+    return gulp.src(dirs.dist.css + '/*.css')
         .pipe(cssnano())
         .pipe(rename(function(path) {
-            path.dirname += "";
-            path.basename += ".min";
-            path.extname = ".css";
+            path.dirname += '';
+            path.basename += '.min';
+            path.extname = '.css';
         }))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest(dirs.dist.css));
 });
 
 // --------------------------------------------------------------------
 
 gulp.task('clean', function() {
-    return gulp.src('dist/css', { read: false })
+    return gulp.src(dirs.dist.css, { read: false })
         .pipe(clean());
+});
+
+// --------------------------------------------------------------------
+
+// Minify any new images
+gulp.task('minifyImages', function() {
+    return gulp.src(dirs.src.img + '/**/*')
+        .pipe(newer(dirs.dist.img))
+        // .pipe(imagemin({ optimizationLevel: 5 }))
+        .pipe(imagemin())
+        .pipe(gulp.dest(dirs.dist.img));
 });
 
 // --------------------------------------------------------------------
@@ -107,9 +139,11 @@ gulp.task('default', function(callback) {
 // --------------------------------------------------------------------
 
 gulp.task('watch', ['build'], function() {
-    // apply sass and concatCSS tasks when any file ending with .scss changes in src/scss dir and sub dirs
-    gulp.watch('src/scss/**/*.scss', ['build']);
+    // apply sass and concatCSS tasks when any file ending with .scss changes in dirs.src.scss dir and sub dirs
+    gulp.watch(dirs.src.scss + '/**/*.scss', ['build']);
 
-    // apply concatJS task when any file ending with .js changes in src/js dir and sub dirs
-    // gulp.watch('src/js/**/*.js', ['concatJS']);
+    gulp.watch(dirs.src.img + '/**/*', ['minifyImages']);
+
+    // apply concatJS task when any file ending with .js changes in dirs.src.js dir and sub dirs
+    // gulp.watch(dirs.src.js + '/**/*.js', ['concatJS']);
 });
